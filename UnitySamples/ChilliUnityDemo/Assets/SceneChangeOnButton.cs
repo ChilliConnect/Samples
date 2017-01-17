b9ecf60cc6cfd73d335feb54ff6879874aa7d395
Scott Downie <scott.downie@tag-games.com>
2017-01-17 09:00:37 +0000

    Adding Main Scene to the demo that allows players to run different demo scenes

---
+++ UnitySamples/ChilliUnityDemo/Assets/SceneChangeOnButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+/// Simple script that changes to the given scene when the sibling button is pressed
+///
+public class SceneChangeOnButton : MonoBehaviour 
+{
+	public string m_sceneName;
+
+	private void Awake () 
+	{
+		transform.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(m_sceneName));
+	}
+}
