cbd61820b6567c452b0a7a7403cdd40fcda336eb
Steve Rennie <stephen.rennie@chilliconnect.com>
2017-03-31 14:02:51 +0100

    Rename update

---
+++ UnitySamples/CharacterGacha/Assets/Gacha/EconomySystem.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChilliConnect;
+using System;
+
+public class EconomySystem 
+{
+	public event System.Action<int> CurrencyBalanceRetrieved = delegate {};
+
+	public event Action<List<Recipe>> OnRecipeListPopulate = delegate {};
+
+	private const string SPAWN_CHARACTER = "SPAWN_CHARACTER";
+
+	private const string CURRENCY_KEY = "COINS";
+
+	private const string RECIPE_TAG = "Recipe";
+
+	/// The list of available teams 
+	/// 
+	public List<Recipe> Recipes { get; set; }
+
+	private static EconomySystem s_singletonInstance = null;
+
+	private ChilliConnectSdk m_chilliConnect;
+
+	public static EconomySystem Get()
+	{
+		return s_singletonInstance;
+	}
+
+	public EconomySystem()
+	{
+		s_singletonInstance = this;
+	}
+
+	public void Initialise(ChilliConnectSdk chilliConnect)
+	{
+		m_chilliConnect = chilliConnect;
+		Recipes = new List<Recipe> ();
+	}
+
+	public void GetRecipeMetaData()
+	{
+		var tags = new List<string>();
+		tags.Add ( RECIPE_TAG );
+
+		var getMetadataDefinitionsRequestDesc = new GetMetadataDefinitionsRequestDesc ();
+		getMetadataDefinitionsRequestDesc.Tags = tags;
+
+		m_chilliConnect.Economy.GetMetadataDefinitions (getMetadataDefinitionsRequestDesc,
+			(request, response) => RenderRecipeList(response), 
+			(request, error) => Debug.LogError (error.ErrorDescription));
+	}
+
+	public void GetPlayerCoinBalance()
+	{
+		var keys = new List<string>();
+		keys.Add ( CURRENCY_KEY );
+
+		var getCurrencyBalanceRequestDesc = new GetCurrencyBalanceRequestDesc ();
+		getCurrencyBalanceRequestDesc.Keys = keys;
+
+		m_chilliConnect.Economy.GetCurrencyBalance (getCurrencyBalanceRequestDesc, 
+			(request, response) => UpdatePlayerCoinDisplay (response), 
+			(request, error) => Debug.LogError (error.ErrorDescription));
+	}
+
+	private void UpdatePlayerCoinDisplay(GetCurrencyBalanceResponse response)
+	{
+		//Update to get by name rather than index, incase there is more than one currency...
+		CurrencyBalanceRetrieved(response.Balances[0].Balance);
+	}
+
+	private void RenderRecipeList(GetMetadataDefinitionsResponse response)
+	{
+		Recipes.Clear ();
+
+		foreach( MetadataDefinition metadataItem in response.Items) {
+			var recipeName = metadataItem.Name;
+			var recipeKey = metadataItem.Key;
+			var itemCustomData = metadataItem.CustomData;
+			var minLvlRange = itemCustomData.AsDictionary ().GetInt ("AttributesMin");
+			var maxLvlRange = itemCustomData.AsDictionary ().GetInt ("AttributesMax");
+			var coinCost = itemCustomData.AsDictionary ().GetInt ("Cost");
+
+			var recipe = new Recipe ();
+			recipe.RecipeName = recipeName;
+			recipe.RecipeKey = recipeKey;
+			recipe.LevelRangeMin = minLvlRange;
+			recipe.LevelRangeMax = maxLvlRange;
+			recipe.CoinCost = coinCost;
+			Recipes.Add( recipe );
+		};
+
+		OnRecipeListPopulate (Recipes);
+	}
+
+	public void CookRecipe(Recipe recipeKey)
+	{
+		var scriptParams = new Dictionary<string, SdkCore.MultiTypeValue> ();
+		scriptParams.Add ("Recipe", recipeKey.RecipeKey);
+
+		var runScriptRequest = new RunScriptRequestDesc(SPAWN_CHARACTER);
+		runScriptRequest.Params = scriptParams;
+
+		m_chilliConnect.CloudCode.RunScript( runScriptRequest, 
+			(request, response) => UnityEngine.Debug.Log("Charcter has been created! Gud Jerb!"),
+			(request, error) => Debug.LogError(error.ErrorDescription) );
+	}
+}
---
+++ UnitySamples/CharacterGacha/Assets/Gacha/RecipeListElementUIController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using ChilliConnect;
+
+/// Controls the elements rendered in the list of teams.
+/// 
+public class RecipeListElementUIController : MonoBehaviour
+{
+	private Recipe m_recipe;
+	private Text m_recipeName;
+	private Text m_recipeKey;
+	private Text m_recipeCost;
+	private GameObject m_createButton;
+	private Image m_background;
+
+	/// Gather the UI
+	/// 
+	private void Awake()
+	{
+		var createButton = transform.FindChild ("CreateCharacterButton").GetComponent<Button> ();
+		createButton.onClick.AddListener (OnCreateButtonClicked);
+
+		m_createButton = createButton.gameObject;
+		m_recipeName = transform.FindChild("RecipeName").GetComponent<Text>();
+		m_recipeKey = transform.FindChild("RecipeKey").GetComponent<Text>();
+		m_recipeCost = transform.FindChild("RecipeCost").GetComponent<Text>();
+		m_background = GetComponent<Image>();
+	}
+
+	/// When the Join button is clicked, use the TeamsSystem to 
+	/// join the team
+	/// 
+	private void OnCreateButtonClicked ()
+	{
+		EconomySystem.Get ().CookRecipe (m_recipe);
+	}
+
+	/// Update the element with the team data
+	///
+	/// @param team
+	/// 	The Team to displau
+	/// 
+	/// @param isPlayersTeam
+	/// 	Is player a memeber of the provided team?
+	/// 
+	/// @param playerHasTeam
+	/// 	Is the player a member of any team?
+	/// 
+	public void Init(Recipe recipe)
+	{
+		m_recipeName.text = recipe.RecipeName;
+		m_recipeKey.text = recipe.RecipeKey;
+		m_recipeCost.text = "Coins: " + recipe.CoinCost.ToString();
+
+		m_recipe = recipe;
+	}
+}
